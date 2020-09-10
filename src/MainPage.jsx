69016307ca55dd393955c2ab37abd41099f9ec95
Cliff-JKL <senya.jkl@gmail.com>
2020-09-11 01:47:12 +0500

    minor dependency optimization

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -135,7 +135,7 @@ const MainPage = () => {
             </div>
           </section>
         </div>
-        <footer className={styles.appFooter}>
+        {/* <footer className={styles.appFooter}>
           <div className={styles.appFooterIcon}>
             <img
               src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='70' height='70'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cpath fill='%23FF005B' d='M0 0h70v70H0z'/%3E%3Cpath fill='%23FFF' d='M52.668 44.375c-2.847 0-5.168-2.303-5.168-5.155 0-2.853 2.321-5.157 5.168-5.157a5.157 5.157 0 010 10.313m-.001-7.501c1.286 0 2.333 1.066 2.333 2.354a2.337 2.337 0 01-2.333 2.334 2.34 2.34 0 01-2.355-2.334 2.356 2.356 0 012.355-2.354zM31.562 57.5v-9.375h2.813V57.5h-2.813zm3.75 0v-9.375h2.813V57.5h-2.813zM46.28 16.935l3.096 3.313-4.126 4.44-1.722-1.873 2.378-2.559-3.093-3.317.004-.004-.004-.007 1.74-1.854.004.007 2.381-2.581 1.74 1.852-2.398 2.583zM17.51 34.063l5.616 9.374h-11.25l5.634-9.374zm.16 5.312l-1.42 1.875h2.813l-1.393-1.875zm12.018-24.034l-3.26 3.242 3.26 3.263L27.8 23.75l-3.269-3.264-3.267 3.264-1.888-1.904 3.27-3.263-3.27-3.242 1.888-1.903 3.267 3.261 3.27-3.262 1.887 1.904z'/%3E%3C/g%3E%3C/svg%3E"
@@ -145,7 +145,8 @@ const MainPage = () => {
           <div className={styles.appFooterTitle}>
             <a className={fonts.display}>DONE BY GROUP CODE 200</a>
           </div>
-        </footer>
+        </footer> */}
+        <footer className={styles.appFooter} />
       </div>
     </>
   );
